a7c27789a80959eb43081b66190fd9b37f3a3c4b
Joan Fabrégat <joan@codeinc.fr>
2018-10-08 19:50:23 +0200

    now uses UUID4 as dir keys

--- src/AssetsMiddleware.php
+++ src/AssetsMiddleware.php
@@ -36,6 +36,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Ramsey\Uuid\Uuid;
 
 
 /**
@@ -110,7 +111,7 @@ class AssetsMiddleware implements MiddlewareInterface
         if ($directoryKey !== null && empty($directoryKey)) {
             throw new EmptyDirectoryKeyException($directoryPath);
         }
-        $this->assetsDirectories[$directoryKey ?? md5($directoryPath)] = $directoryPath;
+        $this->assetsDirectories[$directoryKey ?? Uuid::uuid4()->toString()] = $directoryPath;
     }
 
     /**
